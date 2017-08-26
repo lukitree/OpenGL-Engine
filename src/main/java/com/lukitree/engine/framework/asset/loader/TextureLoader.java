3651c4f493045d05b7e4d52f8bde4a89de7e1e17
lukitree <lukitree@gmail.com>
2017-08-26 05:28:58 -0600

    Adjust mipmap filtering and add maven dep: JBullet

--- src/main/java/com/lukitree/engine/framework/asset/loader/TextureLoader.java
+++ src/main/java/com/lukitree/engine/framework/asset/loader/TextureLoader.java
@@ -73,9 +73,9 @@ public class TextureLoader
 
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, -3);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, -1.1f);
 
 		return new Texture(texture);
 	}
